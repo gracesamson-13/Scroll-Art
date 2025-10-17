58ead02a57da6be438df78de320d2c0a92756dbd
Grace <grace_samson@horacemann.org>
2025-10-16 22:35:48 -0400

    Add files via upload

---
+++ Java_Scroll_Art/Scroll.Art.Penguin.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        penguin();
+    }
+    
+        static char[][] penguin() {
+    char[][] image = new char[peng.height][peng.width];
+    for (int x = 0; x < peng.height; x++) {
+        for (int y = 0; y < peng.width; y++) {
+            image[x][y] = ' ';
+        }
+        }
+
+        System.out.println("
+        
+               ___
+               | |
+              _'_'_
+             ( ^ ^ )
+             /  V  \
+            /(  O  )\
+           } \ | | / {
+               / \
+             ^^   ^^  
+                                  "
+    
+    );
+            
+
+
+    }
+
+
+
+}
---
+++ Java_Scroll_Art/src/README.md
@@ -0,0 +1,5 @@
+## Getting Started
+Name: Grace Samson
+
+Citations: None
+All ASCII art was designed by me with no outside help
